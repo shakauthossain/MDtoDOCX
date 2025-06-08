3512fd271dfa4ccd1a067897cd9aca3e669d8e58
Shakaut Hossain <75905534+shakauthossain@users.noreply.github.com>
2025-06-08 21:55:34 +0600

    Update add-borders.lua

--- add-borders.lua
+++ add-borders.lua
@@ -1,11 +1,7 @@
-function Table(el)
-  el.attributes = el.attributes or {}
-  el.attributes['style'] = 'width:100%;border:1px solid black;border-collapse:collapse'
-  return el
-end
-
-function Cell(el)
-  el.attributes = el.attributes or {}
-  el.attributes['style'] = 'border:1px solid black;padding:6px;'
-  return el
+function Table(elem)
+  elem.attr = elem.attr or {}
+  elem.attr.classes = elem.attr.classes or {}
+  elem.attr.attributes = elem.attr.attributes or {}
+  elem.attr.attributes['border'] = '1'
+  return elem
 end
